ad915baff5b836998af25323cdb3c250ec51a367
Yordan Kyosev <termi3@users.noreply.github.com>
2024-11-30 19:23:40 +0100

    sample adusted

--- pytexlib/textile_test.py
+++ pytexlib/textile_test.py
@@ -4,12 +4,20 @@ from textilelib import *
 
 import math
 
+# one fiber in yarn (monofilament) in x Direction
 fi=fiber()
+fi.diameter=2.2
 fi.setcolour(123,23,230)
 fi.append_point(1,2,3)
-fi.append_point(12,2,3)
-fi.append_point(22,2,3)
+fi.append_point(3,2,3)
+fi.append_point(4,3,3)
+fi.append_point(10,2,3)
 
+# Create Yarn ya
+ya=yarn()
+ya.add_fiber(fi)
+
+# Two fibers along Z Direction
 fi2=fiber()
 fi2.setcolour(123,230,230)
 fi2.diameter=3
@@ -22,17 +30,13 @@ fi3.diameter=2
 for i in range (0,10):
     fi3.append_point(0.2 + 2.8*math.sin(i+3),3*math.cos(i*0.9+3),i)
 
-
-ya=yarn()
-ya.add_fiber(fi)
-
+#Create yarn ya1
 ya1=yarn()
 ya1.add_fiber(fi2)
-   
 ya1.add_fiber(fi3)
 
 t=textile()
-#t.add_yarn(ya)
+t.add_yarn(ya)
 t.add_yarn(ya1)
 t.write_file("mysample.csv")
 
